dae4e8818211c51ea53514fc27dc314e2c3e910b
adnanrahin <adnan.rahin13@gmail.com>
2021-12-22 02:08:17 -0500

    data frame created

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ from pyspark.sql import SparkSession
 from pyspark import SparkContext
 
 if __name__ == "__main__":
-    print("Print")
+    spark = (SparkSession
+             .builder
+             .master("local[*]")
+             .appName("FlightDataAnalysis")
+             .getOrCreate())
+
+    data_source_path = '2015_flights_data'
+
+    flights_csv = spark.sparkContext.textFile(data_source_path + '/flights.csv')
+    airlines_csv = spark.sparkContext.textFile(data_source_path + '/airlines.csv')
+    airport_csv = spark.sparkContext.textFile(data_source_path + '/airports.csv')
+
