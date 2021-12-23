2f2ec7324ccdc16bd094273d5b802c94930d22ba
adnanrahin <adnan.rahin13@gmail.com>
2021-12-22 19:32:31 -0500

    python RDD removed

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
+import faulthandler
+
+
+def load_data_set_to_rdd(path, spark):
+    rdd = (spark
+           .sparkContext
+           .textFile(path)
+           .filter(lambda row: row is not None)
+           .filter(lambda row: row != ""))
+    return rdd
+
 
 if __name__ == "__main__":
+    faulthandler.enable()
+
     spark = (SparkSession
              .builder
              .master("local[*]")
-             .config("spark.executor.memory", "32G")
-             .config("spark.driver.memory", "32G")
+             .config("spark.executor.memory", "6G")
+             .config("spark.driver.memory", "4G")
              .appName("FlightDataAnalysis")
              .getOrCreate())
 
     data_source_path = '2015_flights_data'
 
-    flights_csv = (spark
-                   .sparkContext
-                   .textFile(data_source_path + '/flights.csv')
-                   .filter(lambda row: row is not None)
-                   .filter(lambda row: row != ""))
 
-    airlines_csv = (spark
-                    .sparkContext
-                    .textFile(data_source_path + '/airlines.csv')
-                    .filter(lambda row: row is not None)
-                    .filter(lambda row: row != ""))
-
-    airport_csv = (spark
-                   .sparkContext
-                   .textFile(data_source_path + '/airports.csv')
-                   .filter(lambda x: x is not None)
-                   .filter(lambda x: x != ""))
-
-    print(flights_csv.first())
